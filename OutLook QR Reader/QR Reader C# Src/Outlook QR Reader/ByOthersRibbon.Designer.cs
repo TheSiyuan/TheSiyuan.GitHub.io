f094bc5ef5ca2c65f7f56ebaaafff0fa049760bc
Siyuan Wei <45238365+TheSiyuan@users.noreply.github.com>
2019-07-07 16:37:21 -0600

    QR Code Reader Src Added
    Added source code, still need to do the build and then post the installer.

---
+++ OutLook QR Reader/QR Reader C# Src/Outlook QR Reader/ByOthersRibbon.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Outlook_QR_Reader
+{
+    partial class ByOthersRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public ByOthersRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ByOthersRibbon));
+            this.TabByOthers = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.QRReader_BTN = this.Factory.CreateRibbonButton();
+            this.TabByOthers.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabByOthers
+            // 
+            this.TabByOthers.Groups.Add(this.group1);
+            this.TabByOthers.Label = "ByOthers";
+            this.TabByOthers.Name = "TabByOthers";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.QRReader_BTN);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // QRReader_BTN
+            // 
+            this.QRReader_BTN.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.QRReader_BTN.Image = global::Outlook_QR_Reader.Properties.Resources.scan_qr;
+            this.QRReader_BTN.Label = "QR Reader";
+            this.QRReader_BTN.Name = "QRReader_BTN";
+            this.QRReader_BTN.ShowImage = true;
+            this.QRReader_BTN.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.QRReader_BTN_Click);
+            // 
+            // ByOthersRibbon
+            // 
+            this.Name = "ByOthersRibbon";
+            this.RibbonType = resources.GetString("$this.RibbonType");
+            this.Tabs.Add(this.TabByOthers);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.TabByOthers.ResumeLayout(false);
+            this.TabByOthers.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab TabByOthers;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton QRReader_BTN;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal ByOthersRibbon Ribbon1
+        {
+            get { return this.GetRibbon<ByOthersRibbon>(); }
+        }
+    }
+}
---
+++ OutLook QR Reader/QR Reader C# Src/Outlook QR Reader/ThisAddIn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+using Gma.System.MouseKeyHook;
+
+
+namespace Outlook_QR_Reader
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            
+            
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        public Outlook.Application exampleGetApplication()
+        {
+            Outlook.Application application = this.Application;
+            return application;
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
