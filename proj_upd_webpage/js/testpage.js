32b27d862604d7aba686d661226537eaca1b1767
Siyuan Wei <45238365+TheSiyuan@users.noreply.github.com>
2019-07-14 20:05:01 -0600

    fddd

--- proj_upd_webpage/js/testpage.js
+++ proj_upd_webpage/js/testpage.js
@@ -2,3 +2,26 @@
 var heading = "this is the new heading";
 
 console.log(heading);
+
+function myFunction() {
+document.getElementById("demo").innerHTML = heading;
+document.getElementById("demo").style.fontSize = "40px";
+}
+function loadDoc() {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo1").innerHTML = this.responseText;
+    anotherfunction(this);
+    }
+};
+xhttp.open("GET", "userdefine/content2.xml", true);
+xhttp.send();
+}
+function anotherfunction(xml){
+var xmlDoc = xml.responseXML;
+var x = xmlDoc.getElementsByTagName("NAME");
+//var y = x[0].childNodes[0].nodeValue;
+var y = x.text;
+document.getElementById("TITLE1").innerHTML = y;
+}
