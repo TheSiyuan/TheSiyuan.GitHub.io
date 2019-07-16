554d5e8d7fa2c3b47e3323d080f5d5440b0cd3ff
Siyuan Wei <45238365+TheSiyuan@users.noreply.github.com>
2019-07-15 21:58:22 -0600

    ddd

--- proj_upd_webpage/js/ud_content.js
+++ proj_upd_webpage/js/ud_content.js
@@ -2,7 +2,6 @@ function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo1").innerHTML = this.responseText;
         pushcountent(this);
         }
     };
