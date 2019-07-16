104a1953f855a3d0ba0ab8d5fd0f34bb0f44b97e
Siyuan Wei <45238365+TheSiyuan@users.noreply.github.com>
2019-07-15 20:11:51 -0600

    dddd

---
+++ proj_upd_webpage/js/ud_content.js
@@ -0,0 +1,17 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo1").innerHTML = this.responseText;
+        anotherfunction(this);
+        }
+    };
+    xhttp.open("GET", "userdefine/content2.xml", true);
+    xhttp.send();
+    }
+    function anotherfunction(xml){
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("NAME");
+    var y = x[0].childNodes[0].nodeValue;
+    document.getElementById("TITLE1").innerHTML = y;
+    }
